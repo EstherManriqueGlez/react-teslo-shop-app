ec3e1f0171ab0f1f055854ffc5f667d568b4406b
Esther Manrique <esthermanriqueepam@gmail.com>
2026-02-04 19:27:51 -0600

    Add interfaces, implement product fetching action, and custom hook for data retrieval.

---
+++ src/shop/hooks/useProducts.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProductsAction } from "../actions/get-products.action";
+
+
+export const useProducts = () => {
+  
+  // TODO: add logic here
+
+
+
+
+
+  return useQuery({
+    queryKey: ['products'],
+    queryFn: getProductsAction,
+  });
+}
