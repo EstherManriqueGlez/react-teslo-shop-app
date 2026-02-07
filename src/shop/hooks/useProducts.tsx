3fafbb19cdec27ff828f72cab8c259605f1578f5
Esther Manrique <esthermanriqueepam@gmail.com>
2026-02-07 13:01:16 -0600

    Refactor useProducts and getProductsAction to accept options for pagination.

--- src/shop/hooks/useProducts.tsx
+++ src/shop/hooks/useProducts.tsx
@@ -1,11 +1,24 @@
+import { useSearchParams } from 'react-router';
 import { useQuery } from '@tanstack/react-query';
+
 import { getProductsAction } from '../actions/get-products.action';
 
 export const useProducts = () => {
   // TODO: add logic here
+  const [searchParams] = useSearchParams();
+
+  const limit = searchParams.get('limit') || 9;
+  const page = searchParams.get('page') || 1;
+  const offset = (Number(page) - 1) * Number(limit);
 
   return useQuery({
-    queryKey: ['products'],
-    queryFn: getProductsAction,
+    queryKey: ['products', limit, offset],
+    queryFn: () =>
+      getProductsAction({
+        limit: isNaN(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+      }),
+
+      staleTime: 1000 * 60 * 5, // 5 minutes
   });
 };
