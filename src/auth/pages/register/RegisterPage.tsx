0d918b7d3ffbb240a0668ad6d38d2ea70fc8e06e
Esther Manrique <esthermanriqueepam@gmail.com>
2026-02-19 09:42:56 -0600

    Add registration functionality with API integration and state management.

--- src/auth/pages/register/RegisterPage.tsx
+++ src/auth/pages/register/RegisterPage.tsx
@@ -1,17 +1,49 @@
-import { Link } from 'react-router';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { CustomLogo } from '@/components/custom/CustomLogo';
+import { useAuthStore } from '@/auth/store/auth.store';
+import { toast } from 'sonner';
 
 export const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  const { register} = useAuthStore();
+
+  const [isPosting, setIsPosting] = useState(false);
+
+
+
+  const handleRegister = async (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsPosting(true);
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const fullName = formData.get('fullName') as string;
+
+    const successRegistration = await register(email, password, fullName);
+
+    if (successRegistration) {
+      toast.success('Registro exitoso. Redirigiendo...');
+      navigate('/');
+      return;
+    }
+
+    toast.error('Error al crear la cuenta, intente nuevamente.');
+    setIsPosting(false);
+  }
+
   return (
     <div className={'flex flex-col gap-6'}>
       <Card className='overflow-hidden p-0'>
         <CardContent className='grid p-0 md:grid-cols-2'>
-          <form className='p-6 md:p-8'>
+          <form className='p-6 md:p-8' onSubmit={handleRegister}>
             <div className='flex flex-col gap-6'>
               <div className='flex flex-col items-center text-center'>
                 <CustomLogo />
@@ -23,6 +55,7 @@ export const RegisterPage = () => {
                 <Label htmlFor='fullName'>Nombre completo</Label>
                 <Input
                   id='fullName'
+                  name='fullName'
                   type='text'
                   placeholder='Nombre completo'
                   required
@@ -32,6 +65,7 @@ export const RegisterPage = () => {
                 <Label htmlFor='email'>Correo</Label>
                 <Input
                   id='email'
+                  name='email'
                   type='email'
                   placeholder='mail@google.com'
                   required
@@ -49,12 +83,13 @@ export const RegisterPage = () => {
                 </div>
                 <Input
                   id='password'
+                  name='password'
                   type='password'
                   required
                   placeholder='Contraseña'
                 />
               </div>
-              <Button type='submit' className='w-full'>
+              <Button type='submit' className='w-full' disabled={isPosting}>
                 Crear cuenta
               </Button>
               <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
