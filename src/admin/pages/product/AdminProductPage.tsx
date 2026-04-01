4879083a76863fe67ee9b60610e0e09b354adef8
Esther Manrique <105511137+EstherManriqueGlez@users.noreply.github.com>
2026-03-31 19:08:56 -0600

    refactor: update product page and form to use English labels

--- src/admin/pages/product/AdminProductPage.tsx
+++ src/admin/pages/product/AdminProductPage.tsx
@@ -1,43 +1,23 @@
-// https://github.com/Klerith/bolt-product-editor
-
-import { AdminTitle } from '@/admin/components/AdminTitle';
 import { Navigate, useParams } from 'react-router';
 
-import { useState } from 'react';
-import { X, Plus, Upload, Tag, SaveAll } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Link } from 'react-router';
 import { useProduct } from '@/admin/hooks/useProduct';
 import { CustomFullScreenLoading } from '@/components/custom/CustomFullScreenLoading';
 import { ProductForm } from './ui/ProductForm';
-
-interface Product {
-  id: string;
-  title: string;
-  price: number;
-  description: string;
-  slug: string;
-  stock: number;
-  sizes: string[];
-  gender: string;
-  tags: string[];
-  images: string[];
-}
 
 export const AdminProductPage = () => {
   const { id } = useParams();
 
   const { isLoading, isError, data: product } = useProduct(id || '');
 
-  const title = id === 'new' ? 'Nuevo producto' : 'Editar producto';
+  const title = id === 'new' ? 'New product' : 'Edit product';
   const subtitle =
     id === 'new'
-      ? 'Aquí puedes crear un nuevo producto.'
-      : 'Aquí puedes editar el producto.';
+      ? 'Here you can create a new product.'
+      : 'Here you can edit the product.';
 
   // Validaciones y redirecciones
   if (isError) {
-    return <Navigate to='/admin/products' />;
+    return <Navigate to="/admin/products" />;
   }
 
   if (isLoading) {
@@ -45,7 +25,7 @@ export const AdminProductPage = () => {
   }
 
   if (!product) {
-    return <Navigate to='/admin/products' />;
+    return <Navigate to="/admin/products" />;
   }
 
   return <ProductForm product={product} title={title} subTitle={subtitle} />;
