c519b0a308cc56ed3e7f11d2cdeb8b2e44580cee
Esther Manrique <105511137+EstherManriqueGlez@users.noreply.github.com>
2026-04-14 18:34:07 -0600

    fix: switch from createBrowserRouter to createHashRouter for routing

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { createBrowserRouter, Navigate } from 'react-router';
+import { createHashRouter, Navigate } from 'react-router';
 
 import { ShopLayout } from './shop/layouts/ShopLayout';
 import { HomePage } from './shop/pages/home/HomePage';
@@ -19,7 +19,8 @@ import {
 const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
 const AdminLayout = lazy(() => import('./admin/layouts/AdminLayout'));
 
-export const AppRouter = createBrowserRouter([
+// export const AppRouter = createBrowserRouter([
+export const AppRouter = createHashRouter([
   // Main Routes
   {
     path: '/',
